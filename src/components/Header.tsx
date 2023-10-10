b2be541cf897652e4395cf7755a2e857023fd5bd
João Vicente <joaovsilva70@outlook.come>
2023-10-10 18:32:55 -0300

    fix: tailwind w-full property on Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import logoTodo from "../assets/todo.svg"
 
 export function Header() {
   return (
-    <div className="max-w-[90rem] h-[200px] flex bg-[#0D0D0D] shrink-0 gap-3 items-center justify-center">
+    <div className="w-full h-[200px] flex bg-[#0D0D0D] shrink-0 gap-3 items-center justify-center">
       <img src={logoRocket} width={22} height={36} alt="" />
       <img src={logoTodo} width={92} height={48} alt="" />
     </div>
